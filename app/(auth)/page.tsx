aea78101313b568abab346d5ad7316a97f82d54a
unknown <joshuajulius2030@gmail.com>
2025-03-26 16:30:30 +0100

    created Input component, applied it in auth page. Complete sign field form

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+'use client'
+import Input from '@/components/Input'
+import React, { useState } from 'react'
 
 const Auth = () => {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+
+    const [password, setPassword] = useState('');
+
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black/50 w-full h-full">
@@ -13,8 +20,38 @@ const Auth = () => {
                             Sign in
                         </h2>
                         <div className="flex flex-col gap-4">
-
+                            {/* imported input component, passing parameters as props */}
+                            <Input 
+                            label='Email'
+                            onChange={(ev: any) => setEmail(ev.target.value)}
+                            id='email'
+                            type='email'
+                            value={email}
+                            
+                            />
+                             <Input 
+                            label='Username'
+                            onChange={(ev: any) => setName(ev.target.value)}
+                            id='name'
+                            type='text'
+                            value={name}
+                            
+                            />
+                            <Input 
+                            label='Password'
+                            onChange={(ev: any) => setPassword(ev.target.value)}
+                            id='password'
+                            type='password'
+                            value={password}
+                            
+                            />
                         </div>
+                        <button className='bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700'>
+                            Login
+                        </button>
+                        <p className="text-neutral-500 mt-12">
+                            First time using Netflix? <span className='text-white ml-1 hover:underline cursor-pointer'>Create account</span>
+                        </p>
                     </div>
                 </div>
             </div>
