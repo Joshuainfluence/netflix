410da530f15ad165efcf10e0c72312a01150bfa6
unknown <joshuajulius2030@gmail.com>
2025-03-26 15:21:13 +0100

    added hero and logo image, created auth file

---
+++ app/(auth)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+        <div className="bg-black w-full h-full lg:opacity-50">
+
+        </div>
+    </div>
+  )
+}
+
+export default Auth
