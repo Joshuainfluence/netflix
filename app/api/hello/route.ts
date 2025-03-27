d5420d5cd65203e6c8195297fa6891afc95efd30
unknown <joshuajulius2030@gmail.com>
2025-03-27 13:17:38 +0100

    created structure for api next.js 15

---
+++ app/api/hello/route.ts
@@ -0,0 +1,21 @@
+// import type { NextApiRequest, NextApiResponse } from "next";
+
+// type Data = {
+//     name: string
+// }
+
+// export default function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse<Data>
+
+// ) {
+//     res.status(200).json({ name: 'John Doe' })
+// }
+
+// app/api/hello/route.ts
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest): Promise<NextResponse> {
+  const response = { name: "John Doe" };
+  return NextResponse.json(response); // Automatically sets the Content-Type header
+}
